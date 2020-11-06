119761b46d2cdeaf8e0d44a8accf4a4fab0c80a5
vincent <vmwininlife@gmail.com>
2020-11-06 23:07:59 +0800

    first commit

---
+++ src/components/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<p>This is detail page</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Detail',
+	mounted:function(){
+		// Normally do some initialization For example : call the API
+		console.log('when page is loaded do something')
+	},
+	data(){
+		return {
+			sendData:this.$route.params.sendDate,
+			returnDate:this.$route.params.returnDate,
+			city:this.$route.params.city
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg-black {
+	background-color: black;
+}
+</style>
